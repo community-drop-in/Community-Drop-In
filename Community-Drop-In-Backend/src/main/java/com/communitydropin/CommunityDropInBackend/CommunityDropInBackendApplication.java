2278c784da43f873fdb93066772c4e3a9ee0cb8a
BCox330 <cox.815@aol.com>
2019-07-29 11:22:18 -0400

    Spring backend and frontend file

---
+++ Community-Drop-In-Backend/src/main/java/com/communitydropin/CommunityDropInBackend/CommunityDropInBackendApplication.java
@@ -0,0 +1,13 @@
+package com.communitydropin.CommunityDropInBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityDropInBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityDropInBackendApplication.class, args);
+	}
+
+}
