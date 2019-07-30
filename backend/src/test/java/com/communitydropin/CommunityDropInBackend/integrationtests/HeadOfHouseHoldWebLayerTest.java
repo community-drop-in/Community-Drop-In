9e69f6e6401b41832bb909aca7794b623a2fc68c
Isaacjk614 <isaacjk614@gmail.com>
2019-07-30 15:58:00 -0400

    ended on a failing test - need to fix how we are passing date

---
+++ backend/src/test/java/com/communitydropin/CommunityDropInBackend/integrationtests/HeadOfHouseHoldWebLayerTest.java
@@ -0,0 +1,81 @@
+package com.communitydropin.CommunityDropInBackend.integrationtests;
+
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Optional;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.communitydropin.CommunityDropInBackend.HeadOfHousehold;
+import com.communitydropin.CommunityDropInBackend.HeadOfHouseholdController;
+import com.communitydropin.CommunityDropInBackend.HeadOfHouseholdRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@WebMvcTest(HeadOfHouseholdController.class)
+@RunWith(SpringRunner.class)
+public class HeadOfHouseHoldWebLayerTest {
+
+	@Autowired
+	MockMvc mockMvc;
+	
+	@MockBean
+	HeadOfHouseholdRepository hohRepo;
+	
+	private HeadOfHousehold testHoh;
+	private ObjectMapper mapper = new ObjectMapper();
+	
+	private Calendar date;
+	
+
+	
+	@Before
+	public void setup() {
+		date.set(1999, 11, 23);
+		testHoh = new HeadOfHousehold("", "", "", 123L, false, 2, date);
+	}
+	@Test
+	public void fetchCollectionOfHoh() throws Exception{
+		when(hohRepo.findAll()).thenReturn(Collections.singletonList(testHoh));
+		mockMvc.perform(get("/api/receipients")).andDo(print()).andExpect(status().isOk())
+				.andExpect(content().contentType("application/json;charset=UTF-8"))
+				.andExpect(content().json(mapper.writeValueAsString(Collections.singletonList(testHoh)), true));
+	}
+	@Test
+	public void fetchSingleHoh() throws Exception {
+		when(hohRepo.findById(1L)).thenReturn(Optional.of(testHoh));
+		mockMvc.perform(get("/api/receipients/1")).andDo(print()).andExpect(status().isOk())
+				.andExpect(content().contentType("application/json;charset=UTF-8"))
+				.andExpect(content().json(mapper.writeValueAsString(testHoh), true));
+	}
+	@Test
+	public void addHoh() throws Exception {
+		 when(hohRepo.save(any(HeadOfHousehold.class))).thenReturn(testHoh);
+         when(hohRepo.findAll()).thenReturn(Collections.singletonList(testHoh));
+         mockMvc.perform(post("/api/add-receipient")
+                 .contentType(MediaType.APPLICATION_JSON_UTF8)
+                 .content(mapper.writeValueAsString(testHoh)))
+                 .andExpect(status().isOk())
+                 .andExpect(content().json(mapper.writeValueAsString(Collections.singletonList(testHoh))));
+         }
+
+	}
+
+	
+	
