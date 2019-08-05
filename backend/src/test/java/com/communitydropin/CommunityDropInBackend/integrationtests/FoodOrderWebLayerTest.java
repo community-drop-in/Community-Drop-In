96294608ef561e96fc02a9d5c002ea2b4285f0f5
matthew-laine <matthew.i.laine@gmail.com>
2019-08-05 13:11:45 -0400

    failing test for posting single food order

--- backend/src/test/java/com/communitydropin/CommunityDropInBackend/integrationtests/FoodOrderWebLayerTest.java
+++ backend/src/test/java/com/communitydropin/CommunityDropInBackend/integrationtests/FoodOrderWebLayerTest.java
@@ -1,11 +1,12 @@
 package com.communitydropin.CommunityDropInBackend.integrationtests;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.time.LocalDate;
 import java.util.Collections;
@@ -15,6 +16,7 @@ import java.util.Optional;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.skyscreamer.jsonassert.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -33,6 +35,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @RunWith(SpringRunner.class)
 public class FoodOrderWebLayerTest {
 
+	private static final String FOODORDER2JSON = "{"
+			+ "\"phoneNumber\": 6145551212,"
+			+ "\"date\": \"2019-08-02\""
+			+ "}";
+
 	@Autowired
 	MockMvc mockMvc;
 
@@ -41,12 +48,14 @@ public class FoodOrderWebLayerTest {
 
 	@MockBean
 	FoodOrderRepository foodOrderRepo;
-	
+
 	ObjectMapper mapper = new ObjectMapper();
 
 	private HeadOfHousehold hoh;
 
 	private FoodOrder foodOrder;
+	
+	private FoodOrder foodOrder2;
 
 	List<FoodOrder> foodOrderList;
 
@@ -58,6 +67,7 @@ public class FoodOrderWebLayerTest {
 		foodOrder = new FoodOrder(hoh, LocalDate.of(2001, 1, 1));
 		foodOrderRepo.save(foodOrder);
 		foodOrderList = Collections.singletonList(foodOrder);
+		foodOrder2 = new FoodOrder(hoh, LocalDate.of(2019, 8, 2));
 	}
 
 	@Test
@@ -67,11 +77,21 @@ public class FoodOrderWebLayerTest {
 				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
 				.andExpect(content().json(mapper.writeValueAsString(foodOrderList), true));
 	}
+
 	@Test
 	public void shouldBeAbleToREtrieveSingleFoodOrder() throws Exception {
 		when(foodOrderRepo.findById(1L)).thenReturn(Optional.of(foodOrder));
 		mockMvc.perform(get("/api/food-orders/1")).andDo(print()).andExpect(status().isOk())
-		.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-		.andExpect(content().json(mapper.writeValueAsString(foodOrder), true));
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+				.andExpect(content().json(mapper.writeValueAsString(foodOrder), true));
+	}
+
+	@Test
+	public void shouldBeAbleToPostSingleFoodOrder() throws Exception {
+		FoodOrder foodOrder2 = new FoodOrder(hoh, LocalDate.of(2019, 8, 2));
+		when(foodOrderRepo.save(any(FoodOrder.class))).thenReturn(foodOrder2);
+		mockMvc.perform(post("/api/food-orders").contentType(MediaType.APPLICATION_JSON_UTF8)
+			.content(FOODORDER2JSON)).andExpect(status().isOk());
+//			.andExpect(content().json(mapper.writeValueAsString(foodOrder2)));
 	}
 }
