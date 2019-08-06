7a34fc0431cd2f802c1b2d3b676f262a722cb399
matthew-laine <matthew.i.laine@gmail.com>
2019-08-06 15:35:07 -0400

    test drive info-page.js in accordance with the content section of our John Doe page. Still need to write tests for delivery status and eligibility

--- frontend/cdi-app/src/components/info-page.js
+++ frontend/cdi-app/src/components/info-page.js
@@ -3,7 +3,37 @@ import React from 'react';
 const InfoPage = ({ recipient }) => (
     <div className='container'>
         <h2 className="info-title">{recipient.firstName} {recipient.lastName}</h2>
-        <section className='info-section dob'></section>
+        <section className='info-section dob'>
+            <div className='info-section__content-div'>
+                <h3 className='info-section__content-div__title dob-title'>DOB:</h3>
+                <h4 className='info-section__content-div__content dob-value'>{recipient.dateOfBirth}</h4>
+            </div>
+        </section>
+        <section className='info-section phone'>
+            <div className='info-section__content-div'>
+                <h3 className='info-section__content-div__title phone-title'>Phone:</h3>     
+                <h4 className='info-section__content-div__title phone-value'>
+                    ({recipient.phoneNumber.slice(0,3)})
+                    {recipient.phoneNumber.slice(3,6)}-
+                    {recipient.phoneNumber.slice(6,10)}</h4>                     
+            </div>
+            <button className='info-section__modify-button phone-modify-button'>Change Phone #</button>
+        </section>
+        <section className='info-section address'>
+            <div className='info-section__content-div'>
+                <h3 className='info-section__content-div__title address-title'>Address:</h3>
+                <h4 className='info-section__content-div__content address-value'>{recipient.address}</h4>
+            </div>
+            <button className='info-section__button address-modify-button'>Change Address</button>
+        </section>
+        <section className='info-section'>
+            <div className='info-section__content-div'>
+                <h3 className='info-section__content-div__title size-title'>Household Size:</h3>
+                <h4 className='info-section__content-div__content size-value'>{recipient.houseSize}</h4>
+            </div>
+            <button className='info-section__modify-button size-modify-button'>Change Household Size</button>
+        </section>
+        
     </div>
     );
 
--- frontend/cdi-app/src/components/info-page.test.js
+++ frontend/cdi-app/src/components/info-page.test.js
@@ -20,21 +20,92 @@ describe('Info Page Component', () => {
         ReactDOM.render(<InfoPage recipient={recipient} />, entryPoint);
     });
 
-    test('render container', () =>{
-        expect(entryPoint.querySelector('.container') instanceof HTMLDivElement).toBeTruthy();
+    describe('renders and header', () => {
+        test('render container', () =>{
+            expect(entryPoint.querySelector('.container') instanceof HTMLDivElement).toBeTruthy();
+        });
+    
+        test('render h2 with class info-title and text firstName lastName ', () => {
+            expect(entryPoint.querySelector('.info-title').tagName).toMatch('H2');
+            expect(entryPoint.querySelector('.info-title').textContent).toMatch('John Doe');
+        });
     });
 
-    test('render h2 with class info-title and text firstName lastName ', () => {
-        expect(entryPoint.querySelector('.info-title').tagName).toMatch('H2');
-        expect(entryPoint.querySelector('.info-title').textContent).toMatch('John Doe');
+    describe('info-section & modify-button basic testing', () => {
+        test('info-section should be a section', () => {
+            expect(entryPoint.querySelector('.info-section').tagName).toMatch('SECTION');
+            expect(entryPoint.querySelector('.dob').tagName).toMatch('SECTION');
+        });
+    
+        test('info-section__content-div should be a div', () => {
+            expect(entryPoint.querySelector('.info-section__content-div').tagName).toMatch('DIV');
+        });
+    
+        test('info-section__content-div__title should be an H3 and contain "Phone:" ', () => {
+            expect(entryPoint.querySelector('.info-section__content-div__title').tagName).toMatch('H3');
+        });
+    
+        test('info-section__content-div__content should be an h4', () => {
+            expect(entryPoint.querySelector('.info-section__content-div__content').tagName).toMatch('H4');
+        });
+        
+        test('modify-button should be a button', () => {
+            expect(entryPoint.querySelector('.info-section__modify-button').tagName).toMatch('BUTTON');
+        }); 
     });
 
-    test('info-section phone should be a section', () => {
-        expect(entryPoint.querySelector('.info-section').tagName).toMatch('SECTION');
-        expect(entryPoint.querySelector('.dob').tagName).toMatch('SECTION');
+    describe('dob content tests', () => {
+        test('dob-title should have text "DOB:"', () => {
+            expect(entryPoint.querySelector('.dob-title').textContent).toMatch('DOB:');
+        });
+        
+        test('dob-value should have text of the date 1995-10-08', () => {
+            expect(entryPoint.querySelector('.dob-value').textContent).toMatch('1995-10-08');
+        });
     });
 
-    test('info-section phone should be a div', () => {
+    describe('phone number content tests', () => {
+        test('phone-title should have text Phone:', () => {
+            expect(entryPoint.querySelector('.phone-title').textContent).toMatch('Phone:');
+        });
+        
+        test('phone-value should have number of (614)555-1212', () => {
+            expect(entryPoint.querySelector('.phone-value').textContent).toMatch('(614)555-1212');
+        });
+    
+        test('phone-modify-button should have text "Change Phone #"', () => {
+            expect(entryPoint.querySelector('.phone-modify-button').textContent).toMatch('Change Phone #');
+        });
+    });
 
-    })
+    describe('address content tests', () => {
+        test('address-title should have text Address:', () => {
+            expect(entryPoint.querySelector('.address-title').textContent).toMatch('Address:');
+        });
+    
+        test('address-value should have text "123 Anywhere Street"', () => {
+            expect(entryPoint.querySelector('.address-value').textContent).toMatch('123 Anywhere Street');
+        });
+    
+        test('address-modify-button should have text "Change Address"', () => {
+            expect(entryPoint.querySelector('.address-modify-button').textContent).toMatch('Change Address');
+        });
+    });
+
+    describe('household size content tests', () => {
+        test('size-title should have text "Household Size:', () => {
+            expect(entryPoint.querySelector('.size-title').textContent).toMatch('Household Size:');
+        });
+    
+        test('size-value should have text "4"', () => {
+            expect(entryPoint.querySelector('.size-value').textContent).toMatch('4');
+        });
+    
+        test('size-modify-button should have text "Change Household Size"', () => {
+            expect(entryPoint.querySelector('.size-modify-button').textContent).toMatch('Change Household Size');
+        });
+    });
+
+
+
 });
