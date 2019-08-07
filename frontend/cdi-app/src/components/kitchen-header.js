2084ad9b160bf5aa63ce53bb27193aa98424e25c
Isaacjk614 <isaacjk614@gmail.com>
2019-08-07 09:43:43 -0400

    kitchen header page

---
+++ frontend/cdi-app/src/components/kitchen-header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const KitchenHeader = () => (
+    <header className='kitchen-main-header'>
+        <h1>Recipients</h1>
+        <h1>Household Size</h1>
+    </header>
+    
+
+);
+export default KitchenHeader; 
