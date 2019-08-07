761fc9e6e8fd809ef3fa3d940cc4556362e99999
matthew-laine <matthew.i.laine@gmail.com>
2019-08-07 11:44:20 -0400

    rename hoh-info-h2 and its const

--- frontend/cdi-app/src/components/hoh-info-page.js
+++ frontend/cdi-app/src/components/hoh-info-page.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import InfoH2 from './hoh-info-page-components/hoh-info-h2';
+import InfoNameHeader from './hoh-info-page-components/hoh-info-name';
 import InfoDOB from './hoh-info-page-components/hoh-info-dob';
 import InfoPhone from './hoh-info-page-components/hoh-info-phone';
 import InfoAddress from './hoh-info-page-components/hoh-info-address';
 import InfoSize from './hoh-info-page-components/hoh-info-size';
 import InfoDelivery from './hoh-info-page-components/hoh-info-delivery';
+import InfoOrders from './hoh-info-page-components/hoh-info-recent-orders';
 
 const SingleRecipientPageContent = ({ recipient }) => (
     <div className='container'>
-        <InfoH2 recipient={recipient} />
+        <InfoNameHeader recipient={recipient} />
         <InfoDOB recipient={recipient} />
         <InfoPhone recipient={recipient} />
         <InfoAddress recipient={recipient} />
