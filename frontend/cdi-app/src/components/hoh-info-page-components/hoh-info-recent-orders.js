85df2a2027fb479b8bc2872dc523ff7f98e58b97
matthew-laine <matthew.i.laine@gmail.com>
2019-08-08 10:23:11 -0400

    fix bug to deal with null order list

--- frontend/cdi-app/src/components/hoh-info-page-components/hoh-info-recent-orders.js
+++ frontend/cdi-app/src/components/hoh-info-page-components/hoh-info-recent-orders.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import InfoOrder from './hoh-info-order';
 
+function makeOrderList(orders) {
+    if(!orders) {
+        return [];
+    }
+    return orders;
+}
+
 const InfoOrderList = ({orders}) => (
     <table className='order-table'>
         <thead className='order-table__head'>
-        <th className='order-table-title' colSpan='2'>ORDERS</th>
+            <tr className='order-table__title-row'>
+                <th className='order-table-title' colSpan='2'>ORDERS</th>
+            </tr>
         </thead>
         <tbody className='order-table__body'>
             <tr>
                 <td className='size-td-label'>PEOPLE</td>
                 <td className='date-td-label'>DATE</td>
             </tr>
-            {orders.map(order => <InfoOrder order={order} />)}
+            console.log(orders);
+            {makeOrderList(orders).map(order => <InfoOrder order={order} />)}
         </tbody>
     </table>
 );
