50bdd5fd5e99cb8e8d033fee60f4c4fdeeaa4cfc
KaiHennacy <kchennacy@gmail.com>
2019-08-08 10:23:38 -0400

    app.js as master page

--- frontend/cdi-app/src/index.js
+++ frontend/cdi-app/src/index.js
@@ -7,32 +7,7 @@ import * as serviceWorker from './serviceWorker';
 // import Counter from './components/counter.jsx'
 import Finder from './components/Finder.js'
 
-// const testRecipient = {
-//     id: 1,
-//     lastName: "Schmmo",
-//     phoneNumber: 3304529259,
-//     address: "1770 Pennsylvania Ave",
-//     deliveryStatus: true,
-//     houseSize: 3,
-//     // datesReceived: [],
-//     firstName: "Joe",
-//     dateOfBirth: "1995-10-07"
-// }
-// console.log(testRecipient)
-
-// const apiRecipient = Api().getRequest(Api().getRootURL()+'recipients', recipients => {
-//     ReactDOM.render(<Finder recipients={recipients} />, document.getElementById('root'));
-//     // recipients.forEach(recipient =>{
-//     //     console.log(recipient)
-//     //     console.log(recipient.id)
-//     //     console.log(recipient.lastName)
-//     //     console.log(recipient.phoneNumber)
-//     //     console.log(recipient.address)
-//     // })
-// })
-
-// ReactDOM.render(<Counter />, document.getElementById('root'));
-ReactDOM.render(<Finder />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
