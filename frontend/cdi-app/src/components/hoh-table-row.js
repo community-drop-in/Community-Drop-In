08f2c1e9586543e4ab9462478ab8e720e2448ff6
Isaacjk614 <isaacjk614@gmail.com>
2019-08-10 14:17:53 -0400

    test drive hoh table

---
+++ frontend/cdi-app/src/components/hoh-table-row.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+function DeliveryStatusText (deliveryStatus) {
+    if (deliveryStatus == true) {
+        return 'Yes';
+    }
+    return 'No';
+ };
+
+const HohTableRow = ({ recipient }) => (
+    <tr className='single-recipient-table__row'>
+        <th className='recipient-first-name'>{recipient.firstName}</th>
+        <th className='recipient-last-name'>{recipient.lastName}</th>
+        <th className='recipient-dob'>{recipient.dateOfBirth}</th>
+        <th className='recipient-phone-number'>
+        ({recipient.phoneNumber.slice(0,3)})
+           {recipient.phoneNumber.slice(3,6)}-
+           {recipient.phoneNumber.slice(6,10)}
+        </th>
+        <th className='recipient-address'>{recipient.address}</th>
+        <th className='recipient-household'>{recipient.houseSize}</th>
+        <th className='recipient-delivery'>{DeliveryStatusText(recipient.deliveryStatus)}</th>
+        <th className='recipient-eligible'></th>
+
+    </tr>
+
+)
+
+
+
+
+
+
+export default HohTableRow;
