fd6d185566a9b1c732c66bb9ee83d8e68c08c6e7
Isaacjk614 <isaacjk614@gmail.com>
2019-08-13 14:05:43 -0400

    functioning click handler

--- frontend/cdi-app/src/Api/AppRouter.js
+++ frontend/cdi-app/src/Api/AppRouter.js
@@ -1,82 +1,87 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-function AppRouter(){
+function AppRouter() {
+    const [recipients, setRecipients] = useState([]);
+    const [recipient, setRecipient] = useState({});
+    const [showAllRecipients, setShowAllRecipients] = useState(true);
 
-	const [recipients, setRecipients] = useState([]);
-	const[showAllRecipients, setShowAllRecipients] = useState(true);
-	
-	useEffect(() =>{
-		fetch(getRootURL() + 'recipients')
-		.then(response => response.json())
-		.then(recipients => setRecipients(recipients))
-	}, [])
-	
-	if(recipients !==undefined && showAllRecipients){
-		return (
-            <Router>
-                <Switch>
-                    <Route path='/' render={() => <HohTable recipients={recipients} isAuthed={true}/>}/>
-                </Switch>
-            </Router>
-		)
-	}
+
+    useEffect(() => {
+        fetch(getRootURL() + 'recipients')
+            .then(response => response.json())
+            .then(recipients => setRecipients(recipients))
+    }, [])
+
+    function handleRecipientClick(recipient) {
+        setRecipient(recipient)
+        console.log(recipient)
+    }
+
+    return (
+        <Router>
+            <Switch>
+                <Route path='/' render={() => <HohTable recipients={recipients} handleRecipientClick={handleRecipientClick} isAuthed={true} />} />
+                <Route path='/single-hoh' render={() => <SingleRecipientPageContent recipient={recipient} isAuthed={true} />} />
+            </Switch>
+        </Router>
+    )
 }
 
-function DeliveryStatusText (deliveryStatus) {
+
+function DeliveryStatusText(deliveryStatus) {
     if (deliveryStatus === true) {
         return 'Yes';
     }
     return 'No';
- }
+}
 
 function getRootURL() {
-	return 'http://localhost:8080/api/';
+    return 'http://localhost:8080/api/';
 }
 
-function HohTable({recipients}) {
-	return(
-		<table className='results-table'>
-		<tbody className='results-table__body'>
-			<tr className='results-table__head'>
-				<th className='head__firstname'>First Name</th>
-				<th className='head__lastname'>Last Name</th>
-				<th className='head__dob'>DOB</th>
-				<th className='head__phone'>Phone #</th>
-				<th className='head__address'>Address</th>
-				<th className='head__household'>Household</th>
-				<th className='head__delivery'>Delivery</th>
-				<th className='head__eligible'>Eligible</th>
-			</tr>
-            {console.log(recipients)}
-			{recipients.map(recipient => <HohTableRow recipient={recipient}/>)}
-		</tbody>
-	</table>
-	)
-}
-
-function HohTableRow({recipient}) {
+function HohTable({ recipients, handleRecipientClick }) {
     return (
-    <tr className='single-recipient-table__row' 
-        onClick={() => {alert('you clicked on '+recipient.firstName)}}
-        >
-        <th className='recipient-first-name'>{recipient.firstName}</th>
-        <th className='recipient-last-name'>{recipient.lastName}</th>
-        <th className='recipient-dob'>{recipient.dateOfBirth}</th>
-        <th className='recipient-phone-number'>
-        {'(' + recipient.phoneNumber.toString().slice(0,3) + ') ' +
-            recipient.phoneNumber.toString().slice(3,6) + '-' +
-            recipient.phoneNumber.toString().slice(6,10)}
-        </th>
-        <th className='recipient-address'>{recipient.address}</th>
-        <th className='recipient-household'>{recipient.houseSize}</th>
-        <th className='recipient-delivery'>{DeliveryStatusText(recipient.deliveryStatus)}</th>
-        <th className='recipient-eligible'></th>
-    </tr>
+        <table className='results-table'>
+            <tbody className='results-table__body'>
+                <tr className='results-table__head'>
+                    <th className='head__firstname'>First Name</th>
+                    <th className='head__lastname'>Last Name</th>
+                    <th className='head__dob'>DOB</th>
+                    <th className='head__phone'>Phone #</th>
+                    <th className='head__address'>Address</th>
+                    <th className='head__household'>Household</th>
+                    <th className='head__delivery'>Delivery</th>
+                    <th className='head__eligible'>Eligible</th>
+                </tr>
+                {recipients.map(recipient => <HohTableRow recipient={recipient} handleRecipientClick={handleRecipientClick} />)}
+            </tbody>
+        </table>
     )
 }
 
-function InfoAddress({address}) {
+function HohTableRow({ recipient, handleRecipientClick }) {
+    return (
+        <tr className='single-recipient-table__row'
+        onClick={() => {handleRecipientClick(recipient)}} 
+        >
+            <th className='recipient-first-name'>{recipient.firstName}</th>
+            <th className='recipient-last-name'>{recipient.lastName}</th>
+            <th className='recipient-dob'>{recipient.dateOfBirth}</th>
+            <th className='recipient-phone-number'>
+                {'(' + recipient.phoneNumber.toString().slice(0, 3) + ') ' +
+                    recipient.phoneNumber.toString().slice(3, 6) + '-' +
+                    recipient.phoneNumber.toString().slice(6, 10)}
+            </th>
+            <th className='recipient-address'>{recipient.address}</th>
+            <th className='recipient-household'>{recipient.houseSize}</th>
+            <th className='recipient-delivery'>{DeliveryStatusText(recipient.deliveryStatus)}</th>
+            <th className='recipient-eligible'></th>
+        </tr>
+    )
+}
+
+function InfoAddress({ address }) {
     return (
         <section className='info-section address'>
             <div className='info-section__content-div'>
@@ -86,21 +91,21 @@ function InfoAddress({address}) {
             <button className='info-section__button address-modify-button'>Change Address</button>
         </section>
     );
-} 
+}
 
-function InfoDelivery({deliveryStatus}) {
+function InfoDelivery({ deliveryStatus }) {
     return (
         <section className='info-section delivery-status'>
-        <div className='info-section__content-div'>
-            <h3 className='info-section__content-div__title delivery-status-title'>Delivery:</h3>
-            <h4 className='info-section__content-div__content delivery-status-value'>{DeliveryStatusText(deliveryStatus)}</h4>
-        </div>
-        <button className='info-section__modify-button'>Remove Delivery</button>
-    </section>
-    ); 
-} 
+            <div className='info-section__content-div'>
+                <h3 className='info-section__content-div__title delivery-status-title'>Delivery:</h3>
+                <h4 className='info-section__content-div__content delivery-status-value'>{DeliveryStatusText(deliveryStatus)}</h4>
+            </div>
+            <button className='info-section__modify-button'>Remove Delivery</button>
+        </section>
+    );
+}
 
-function InfoDOB({dateOfBirth}) {
+function InfoDOB({ dateOfBirth }) {
     return (
         <section className='info-section dob'>
             <div className='info-section__content-div'>
@@ -109,24 +114,24 @@ function InfoDOB({dateOfBirth}) {
             </div>
         </section>
     );
-} 
+}
 
-function InfoNameHeader({recipient}){
+function InfoNameHeader({ recipient }) {
     return (
         <h2 className="info-title">{recipient.firstName} {recipient.lastName}</h2>
-        );
-} 
+    );
+}
 
-function InfoOrder({order}) {
+function InfoOrder({ order }) {
     return (
         <tr className='order-row'>
             <td className='size-td'>{order.size}</td>
             <td className='date-td'>{order.date}</td>
         </tr>
-    );    
-} 
+    );
+}
 
-function InfoOrderList ({orders}) {
+function InfoOrderList({ orders }) {
     return (
         <table className='order-table'>
             <thead className='order-table__head'>
@@ -144,36 +149,36 @@ function InfoOrderList ({orders}) {
             </tbody>
         </table>
     );
-} 
+}
 
-function InfoPhone({phoneNumber}) {
+function InfoPhone({ phoneNumber }) {
     return (
         <section className='info-section phone'>
-        <div className='info-section__content-div'>
-            <h3 className='info-section__content-div__title phone-title'>Phone:</h3>     
-            <h4 className='info-section__content-div__title phone-value'>
-                ({phoneNumber.slice(0,3)})
-                {phoneNumber.slice(3,6)}-
-                {phoneNumber.slice(6,10)}</h4>                     
-        </div>
-        <button className='info-section__modify-button phone-modify-button'>Change Phone #</button>
-    </section>
+            <div className='info-section__content-div'>
+                <h3 className='info-section__content-div__title phone-title'>Phone:</h3>
+                <h4 className='info-section__content-div__title phone-value'>
+                    ({phoneNumber.slice(0, 3)})
+                {phoneNumber.slice(3, 6)}-
+                {phoneNumber.slice(6, 10)}</h4>
+            </div>
+            <button className='info-section__modify-button phone-modify-button'>Change Phone #</button>
+        </section>
     );
-} 
+}
 
-function InfoSize({houseSize}) {
+function InfoSize({ houseSize }) {
     return (
         <section className='info-section size'>
-        <div className='info-section__content-div'>
-            <h3 className='info-section__content-div__title size-title'>Household Size:</h3>
-            <h4 className='info-section__content-div__content size-value'>{houseSize}</h4>
-        </div>
-        <button className='info-section__modify-button size-modify-button'>Change Household Size</button>
-    </section>
+            <div className='info-section__content-div'>
+                <h3 className='info-section__content-div__title size-title'>Household Size:</h3>
+                <h4 className='info-section__content-div__content size-value'>{houseSize}</h4>
+            </div>
+            <button className='info-section__modify-button size-modify-button'>Change Household Size</button>
+        </section>
     );
-} 
+}
 
-function SingleRecipientPageContent({recipient}) {
+function SingleRecipientPageContent({ recipient }) {
     return (
         <div className='container'>
             <InfoNameHeader recipient={recipient} />
@@ -184,7 +189,7 @@ function SingleRecipientPageContent({recipient}) {
             <InfoDelivery deliveryStatus={recipient.deliveryStatus} />
             <InfoOrderList orders={recipient.foodOrders} />
         </div>
-        );
+    );
 }
 
 export default AppRouter
