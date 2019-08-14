47b50fd1e24b99913dee48ce32a860d52608dcc9
BCox330 <cox.815@aol.com>
2019-08-14 15:33:14 -0400

    Fixed form page to be recognized by the back end

--- frontend/cdi-app/src/Api/AppRouter.js
+++ frontend/cdi-app/src/Api/AppRouter.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import SingleRecipientPageContent from '../components/hoh-info-page'
 import HohTable from '../components/all-hoh-table'
+import HohForm from '../components/hoh-form-page/hoh-form'
 
 function AppRouter() {
     const [recipients, setRecipients] = useState([]);
@@ -16,12 +17,24 @@ function AppRouter() {
     function handleRecipientClick(recipient) {
         setRecipient(recipient)
     }
+    function submitNewRecipient(model) { console.log(JSON.stringify(model)) }
 
     return (
         <Router>
             <Switch>
                 <Route exact path='/' render={() => <HohTable recipients={recipients} handleRecipientClick={handleRecipientClick} />} />
                 <Route path='/single-hoh' render={() => <SingleRecipientPageContent recipient={recipient} />} />
+                <Route path='/hoh-form' render={() => <HohForm model={{
+                    lastName: "",
+                    phoneNumber: 0,
+                    address: "",
+                    deliveryStatus: null,
+                    houseSize: 0,
+                    dateOfBirth: "",
+                    foodOrders: [],
+                    firstName: ""
+                }}
+                    submitNewRecipient={submitNewRecipient} />} />
             </Switch>
         </Router>
     )
