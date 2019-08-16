5e52dd77decf48b82abaad6b5b30c04dda86e7aa
KaiHennacy <kchennacy@gmail.com>
2019-08-16 15:07:47 -0400

    Commit to save before yarn init

--- backend/src/main/resources/static/js/queueView.js
+++ backend/src/main/resources/static/js/queueView.js
@@ -1,7 +1,35 @@
 
+console.log('js running');
 
-Class Queue{
+const queue = new Queue();
+
+queue.renderRecipients();
+
+class Queue{
+
     renderRecipients(){
-
+        this.getQueueDiv().addChild(
+            document.createElement('div')
+            .addAttribute('th:each', 'order: ${orders}')
+            .classList.add('kitchen-sub-header')
+            .addChild(
+                document.createElement('h2')
+                .addAttribute('th:text', "${order.hohFirstName} + ' ' + ${order.hohLastName}")
+                .classList.add('recipient-kitchen-name')
+            ).addChild(
+                document.createElement('h2')
+                .addAttribute('th:text', "${order.size}")
+                .classList.add('recipient-kitchen-name')
+            )
+        )
+    }
+    getQueueDiv(){
+        return document.querySelector('queue')
     }
 }
+
+{/* <div class='kitchen-sub-header' th:each="order: ${orders}">
+        <h2 class='recipient-kitchen-name'th:text="${order.hohFirstName} + ' ' + ${order.hohLastName}"></h2>
+        <h2 class='recipient-kitchen-house-size' th:text="${order.size}"></h2>
+</div> */}
+
