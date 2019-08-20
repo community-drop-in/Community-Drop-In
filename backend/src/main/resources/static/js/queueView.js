d0f6239332be7af52463d6077943539eda918251
KaiHennacy <kchennacy@gmail.com>
2019-08-20 10:20:36 -0400

    Update style of queueView

--- backend/src/main/resources/static/js/queueView.js
+++ backend/src/main/resources/static/js/queueView.js
@@ -27,7 +27,6 @@ class Queue {
             fetch("http://localhost:8080/queue/delete/" + inputId, {
               method: "DELETE"
             });
-            alert("Order Served");
           });
         });
       });
